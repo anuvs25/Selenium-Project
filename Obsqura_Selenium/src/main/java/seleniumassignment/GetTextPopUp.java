27d8203f0370d5a086a91b1337530ec15439a739
HP <HP@192.168.1.36>
2024-03-02 11:07:49 +0530

    Commit

--- Obsqura_Selenium/src/main/java/seleniumassignment/GetTextPopUp.java
+++ Obsqura_Selenium/src/main/java/seleniumassignment/GetTextPopUp.java
@@ -7,15 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetTextPopUp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		//launch url= https://the-internet.herokuapp.com/entry_ad print 
 		//text of opened pop up "THIS IS A MODAL WINDOW" and also click on "close " link
+		
 		System.setProperty("webdriver.chrome.driver", "D:\\Testing\\ObsquraSelenium\\LocalDrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/entry_ad");
-//		driver.manage().window().maximize();
-//		driver.switchTo().alert();
+		driver.manage().window().maximize();
+
+		
 		WebElement pop_up_txt = driver.findElement(By.xpath("//div[@class='modal-title']"));
+		Thread.sleep(3000);
 		System.out.println(pop_up_txt.getText());
 		
 
